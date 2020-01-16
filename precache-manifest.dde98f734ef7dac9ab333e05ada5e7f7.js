fa76db8132ec82e7b34a54a9c1c5afd812aa01b3
Uedson Reis <uedsonreis@MacReis.local>
2020-01-16 09:00:00 -0300

    Testando o Senhador App.

---
+++ precache-manifest.dde98f734ef7dac9ab333e05ada5e7f7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d95923222857c86540576e127ddb1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095971af70e17ef61219",
+    "url": "/static/css/3.e0f850a3.chunk.css"
+  },
+  {
+    "revision": "1de180536ef1eda10bb4",
+    "url": "/static/css/4.b310fb38.chunk.css"
+  },
+  {
+    "revision": "30b1c0dd1cf82270ba5f",
+    "url": "/static/css/5.b310fb38.chunk.css"
+  },
+  {
+    "revision": "6c52c77918a8f3adc19c",
+    "url": "/static/css/main.2a7bf81b.chunk.css"
+  },
+  {
+    "revision": "149672c9123d6f0362a2",
+    "url": "/static/js/0.5e36addb.chunk.js"
+  },
+  {
+    "revision": "095971af70e17ef61219",
+    "url": "/static/js/3.d1f3472e.chunk.js"
+  },
+  {
+    "revision": "21ce168a969a6e07f57f36c27b5deae4",
+    "url": "/static/js/3.d1f3472e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1de180536ef1eda10bb4",
+    "url": "/static/js/4.d24fa070.chunk.js"
+  },
+  {
+    "revision": "30b1c0dd1cf82270ba5f",
+    "url": "/static/js/5.917e9494.chunk.js"
+  },
+  {
+    "revision": "6c52c77918a8f3adc19c",
+    "url": "/static/js/main.9f338749.chunk.js"
+  },
+  {
+    "revision": "49187783b40cdccf6a21",
+    "url": "/static/js/runtime-main.1cec090c.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "19cb9733631c9f641868cf7347b38c78",
+    "url": "/static/media/background.19cb9733.png"
+  },
+  {
+    "revision": "de2279ff0d5cfcd22e7c67572a03489d",
+    "url": "/static/media/logo.de2279ff.png"
+  },
+  {
+    "revision": "d8faad9e1f7bac27d71085cf9beaac71",
+    "url": "/static/media/logo_pcb.d8faad9e.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/static/media/sprite-flags-24x24.7ed12ca4.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
